e4e8bb938e01efbd8eac6780d6b87f705f6c01df
Marko Eltermann <markoeltermann@gmail.com>
2025-08-23 14:55:15 +0300

    Web optimisations

---
+++ Web/Web.Client/Helpers/TrimmerPreserve.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Web.Client.Helpers;
+
+public static class TrimmerPreserve
+{
+    // This ensures MinValue and MaxValue are preserved even with trimming
+    [DynamicDependency("MinValue", typeof(double))]
+    [DynamicDependency("MaxValue", typeof(double))]
+    private static void PreserveDoubleFields() { }
+}
