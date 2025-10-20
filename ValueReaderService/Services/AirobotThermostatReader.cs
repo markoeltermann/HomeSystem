a7ce9a52662086186c86bc505f60c9bb50828879
Marko Eltermann <markoeltermann@gmail.com>
2025-10-20 23:10:43 +0300

    Airobot thermostat getSettings support

--- ValueReaderService/Services/AirobotThermostatReader.cs
+++ ValueReaderService/Services/AirobotThermostatReader.cs
@@ -25,11 +25,12 @@ public class AirobotThermostatReader(
         if (address?.IP is null)
             return null;
 
-        var url = $"http://{address.IP}";
+        var baseUrl = $"http://{address.IP}";
         if (address.Port is not null)
-            url += ":" + address.Port;
+            baseUrl += ":" + address.Port;
 
-        url += "/api/thermostat/getStatuses";
+        var statusesUrl = baseUrl + "/api/thermostat/getStatuses";
+        var settingsUrl = baseUrl + "/api/thermostat/getSettings";
 
         using var httpClient = httpClientFactory.CreateClient(nameof(AirobotThermostatReader));
 
@@ -37,63 +38,97 @@ public class AirobotThermostatReader(
         var base64EncodedAuthenticationString = Convert.ToBase64String(Encoding.ASCII.GetBytes(authenticationString));
         httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
 
-        var response = await httpClient.GetAsync(url);
-        var responseText = await response.Content.ReadAsStringAsync();
-
-        var jDoc = JsonDocument.Parse(responseText);
+        var jDocStatuses = await GetJDoc(statusesUrl, httpClient);
+        var jDocSettings = await GetJDoc(settingsUrl, httpClient);
 
         var result = new List<PointValue>(devicePoints.Count);
         foreach (var point in devicePoints)
         {
-            var addressParts = point.Address.Split('*');
-            if (addressParts.Length is 1 or 2)
+            var value = GetPointValue(jDocStatuses, point);
+            if (value != null)
             {
-                JsonElement? element = jDoc.RootElement;
-                var addressPathParts = addressParts[0].Split('.');
-                foreach (var pathSegment in addressPathParts)
+                result.Add(value);
+            }
+            else
+            {
+                value = GetPointValue(jDocSettings, point);
+                if (value != null)
                 {
-                    if (element != null)
-                    {
-                        var propertyName = pathSegment;
-                        var arrayIndex = (int?)null;
-                        if (pathSegment.EndsWith(']'))
-                        {
-                            var pt = pathSegment.TrimEnd(']').Split('[');
-                            propertyName = pt[0];
-                            arrayIndex = int.Parse(pt[1]);
-                        }
-
-                        if (element.Value.TryGetProperty(propertyName, out var t))
-                        {
-                            element = t;
-                            if (arrayIndex != null)
-                                element = element.Value[arrayIndex.Value];
-                        }
-                        else
-                            element = null;
-                    }
-                }
-
-                if (element != null)
-                {
-                    if (point.DataType.Name == "Float" && element.Value.TryGetDecimal(out var value))
-                    {
-                        var multiplier = addressParts.Length == 2 ? decimal.Parse(addressParts[1], InvariantCulture) : 1.0m;
-                        result.Add(new(point, (value * multiplier).ToString(InvariantCulture)));
-                    }
-                    else if (point.DataType.Name == "Boolean")
-                    {
-                        try
-                        {
-                            var b = element.Value.GetInt32();
-                            result.Add(new(point, (b > 0).ToString()));
-                        }
-                        catch { }
-                    }
+                    result.Add(value);
                 }
             }
         }
 
         return result;
+    }
+
+    private static PointValue? GetPointValue(JsonDocument jDoc, DevicePoint point)
+    {
+        var addressParts = point.Address.Split('*');
+        if (addressParts.Length is 1 or 2)
+        {
+            JsonElement? element = jDoc.RootElement;
+            var addressPathParts = addressParts[0].Split('.');
+            foreach (var pathSegment in addressPathParts)
+            {
+                if (element != null)
+                {
+                    var propertyName = pathSegment;
+                    var arrayIndex = (int?)null;
+                    if (pathSegment.EndsWith(']'))
+                    {
+                        var pt = pathSegment.TrimEnd(']').Split('[');
+                        propertyName = pt[0];
+                        arrayIndex = int.Parse(pt[1]);
+                    }
+
+                    if (element.Value.TryGetProperty(propertyName, out var t))
+                    {
+                        element = t;
+                        if (arrayIndex != null)
+                            element = element.Value[arrayIndex.Value];
+                    }
+                    else
+                        element = null;
+                }
+            }
+
+            if (element != null)
+            {
+                if (point.DataType.Name == "Float" && element.Value.TryGetDecimal(out var value))
+                {
+                    var multiplier = addressParts.Length == 2 ? decimal.Parse(addressParts[1], InvariantCulture) : 1.0m;
+                    return new(point, (value * multiplier).ToString(InvariantCulture));
+                }
+                else if (point.DataType.Name == "Boolean")
+                {
+                    try
+                    {
+                        var b = element.Value.GetInt32();
+                        return new(point, (b > 0).ToString());
+                    }
+                    catch { }
+                }
+                else if (point.DataType.Name == "Enum")
+                {
+                    try
+                    {
+                        var b = element.Value.GetInt32();
+                        return new(point, b.ToString());
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static async Task<JsonDocument> GetJDoc(string baseUrl, HttpClient httpClient)
+    {
+        using var response = await httpClient.GetAsync(baseUrl);
+        var responseText = await response.Content.ReadAsStringAsync();
+        var jDoc = JsonDocument.Parse(responseText);
+        return jDoc;
     }
 }
